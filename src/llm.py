badcabc62d31fdb1103b53cd03293b099207db1b
ANIK PAUL <141066625+anikpaul00@users.noreply.github.com>
2026-07-31 17:24:19 +0600

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -5,6 +5,8 @@ def get_llm():
     
     llm = ChatGroq(
         model="qwen/qwen3.6-27b", # can also use llama-3.1-8b-instant, openai/gpt-oss-20b
+        reasoning_format="hidden",
+        reasoning_effort="none", # can degrade performance
         temperature=0.1,
         api_key=st.secrets["GROQ_API_KEY"]
     )
