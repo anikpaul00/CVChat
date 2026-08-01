cf6a02eb403cfe233ffbefe9acdc9dbf2d28d072
ANIK PAUL <141066625+anikpaul00@users.noreply.github.com>
2026-08-01 21:19:15 +0600

    Update llm.py

--- src/llm.py
+++ src/llm.py
@@ -7,8 +7,8 @@ def get_llm():
         model="openai/gpt-oss-20b", # can also use llama-3.1-8b-instant, openai/gpt-oss-20b, qwen/qwen3.6-27b
         max_completion_tokens=1024,
         reasoning_format="hidden",
-        reasoning_effort="medium", # can degrade performance
-        temperature=0.3,
+        reasoning_effort="low", # can degrade performance
+        temperature=0.2,
         stream=True,
         api_key=st.secrets["GROQ_API_KEY"]
     )
